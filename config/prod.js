6d076f023c4783580ca771ba0b03d7a7168259a8
Michael Klein <miketheklein@gmail.com>
2019-02-18 10:48:41 -0800

    finished auth flow testing heroku

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: "process.env.GOOGLE_CLIENT_ID",
+  googleClientSecret: "process.env_GOOGLE_CLIENT_SECRET",
+  mongoURI: 'process.env.MONGO_URI',
+  atlasPrivateKey: 'process.env.ATLAS_PRIVATE_KEY',
+  atlasPublicKey: 'process.env.ATLAS_PUBLIC_KEY',
+  cookieKey: 'process.env.COOKIE_KEY',
+};
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const keys = require('./config/keys')
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const express = require('express');
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true }).then(() => {
+  console.log('connected!')
+}).catch((err) => {
+  console.log("Not Connected to Database ERROR! ", err);
+});;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there buddy' })
-})
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
