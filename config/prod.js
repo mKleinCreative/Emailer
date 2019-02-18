b7c0f013c279e6e5aebd3af330132b562db8cc9d
Michael Klein <miketheklein@gmail.com>
2019-02-18 12:59:48 -0800

    messing around with config issues

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 module.exports = {
-  googleClientID: 'process.env.GOOGLE_CLIENT_ID',
-  googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
-  mongoURI: 'process.env.MONGO_URI',
-  atlasPrivateKey: 'process.env.ATLAS_PRIVATE_KEY',
-  atlasPublicKey: 'process.env.ATLAS_PUBLIC_KEY',
-  cookieKey: 'process.env.COOKIE_KEY',
+  atlasPrivateKey: process.env.ATLAS_PRIVATE_KEY,
+  atlasPublicKey: process.env.ATLAS_PUBLIC_KEY,
+  cookieKey: process.env.COOKIE_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI
 };
