668e2204a9b09725344d35abd3799543acbcc401
Michael Klein <miketheklein@gmail.com>
2019-03-04 08:12:07 -0800

    adding billing and client side applicaiton

--- index.js
+++ index.js
@@ -3,28 +3,39 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const express = require('express');
+const bodyParser = require('body-parser');
 require('./models/user');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true }).then(() => {
-  console.log('connected!')
-}).catch((err) => {
-  console.log("Not Connected to Database ERROR! ", err);
-});;
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true }).catch(
+  (err) => { console.log('MONGOOSE err (╯°□°)╯︵ ┻━┻ ', err) }
+);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
 )
-
 app.use(passport.initialize())
 app.use(passport.session())
 
+if (process.env.NODE_ENV === 'production') {
+  // serves up React app 
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 require('./routes/authRoutes')(app)
+require('./routes/billingRoutes')(app)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,11 +8,19 @@ module.exports = (app) => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/auth/google/callback',
+    passport.authenticate('google'),
+    (err, req, res, next) => {
+      if (err.name === 'TokenError') {
+        res.redirect('/auth/google');
+      }
+      res.redirect('/surveys');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect('/')
   })
 
   app.get('/api/current_user', (req, res) => {
